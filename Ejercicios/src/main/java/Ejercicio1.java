c4b1babe28cbe8d7c32595bee9da40102005ed6c
rodrigoyr <ryeperub@myuax.com>
2024-04-15 12:10:26 +0200

    commit

--- Ejercicios/src/main/java/Ejercicio1.java
+++ Ejercicios/src/main/java/Ejercicio1.java
@@ -1,31 +1,27 @@
-public class Ejercicios1<T> {
-    private T primerElemento;
-    private T segundoElemento;
-
-    // Constructor
-    public Pareja(T primerElemento, T segundoElemento) {
-        this.primerElemento = primerElemento;
-        this.segundoElemento = segundoElemento;
-    }
-
-    // Método para obtener el primer elemento
-    public T obtenerPrimerElemento() {
-        return primerElemento;
-    }
-
-    // Método para obtener el segundo elemento
-    public T obtenerSegundoElemento() {
-        return segundoElemento;
-    }
+public class Ejercicio1 {
 
     public static void main(String[] args) {
         // Ejemplo de uso
-        Pareja<Integer> parejaEnteros = new Pareja<>(5, 10);
-        System.out.println("Primer elemento: " + parejaEnteros.obtenerPrimerElemento());
-        System.out.println("Segundo elemento: " + parejaEnteros.obtenerSegundoElemento());
+        Pareja<Integer, String> pareja = new Pareja<>(10, "Hola");
+        System.out.println("Primer dato: " + pareja.getPrimero());
+        System.out.println("Segundo dato: " + pareja.getSegundo());
+    }
 
-        Pareja<String> parejaStrings = new Pareja<>("Hola", "Mundo");
-        System.out.println("Primer elemento: " + parejaStrings.obtenerPrimerElemento());
-        System.out.println("Segundo elemento: " + parejaStrings.obtenerSegundoElemento());
+    public static class Pareja<T, S> {
+        private T primero;
+        private S segundo;
+
+        public Pareja(T primero, S segundo) {
+            this.primero = primero;
+            this.segundo = segundo;
+        }
+
+        public T getPrimero() {
+            return primero;
+        }
+
+        public S getSegundo() {
+            return segundo;
+        }
     }
 }
