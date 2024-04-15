c6accbc5fbafe8c19766e5993812e391a8284426
rodrigoyr <ryeperub@myuax.com>
2024-04-15 12:09:16 +0200

    commit

---
+++ Ejercicios/src/main/java/Ejercicio1.java
@@ -0,0 +1,31 @@
+public class Ejercicios1 {
+    private T primerElemento;
+    private T segundoElemento;
+
+    // Constructor
+    public Pareja(T primerElemento, T segundoElemento) {
+        this.primerElemento = primerElemento;
+        this.segundoElemento = segundoElemento;
+    }
+
+    // Método para obtener el primer elemento
+    public T obtenerPrimerElemento() {
+        return primerElemento;
+    }
+
+    // Método para obtener el segundo elemento
+    public T obtenerSegundoElemento() {
+        return segundoElemento;
+    }
+
+    public static void main(String[] args) {
+        // Ejemplo de uso
+        Pareja<Integer> parejaEnteros = new Pareja<>(5, 10);
+        System.out.println("Primer elemento: " + parejaEnteros.obtenerPrimerElemento());
+        System.out.println("Segundo elemento: " + parejaEnteros.obtenerSegundoElemento());
+
+        Pareja<String> parejaStrings = new Pareja<>("Hola", "Mundo");
+        System.out.println("Primer elemento: " + parejaStrings.obtenerPrimerElemento());
+        System.out.println("Segundo elemento: " + parejaStrings.obtenerSegundoElemento());
+    }
+}
